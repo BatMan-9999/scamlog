c13e7bef15604e2fb15fedcd38e18d8f0e8746d2
Jim <compositr@compositr.dev>
2022-07-12 09:14:08 +1000

    feat: augment nextauth types
    Signed-off-by: Jim <compositr@compositr.dev>

---
+++ src/modules/auth/types/next-auth.d.ts
@@ -0,0 +1,11 @@
+import { AdminUser } from "@prisma/client";
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  type Session = DefaultSession & {
+    admin: boolean | AdminUser;
+  };
+}
