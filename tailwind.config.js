fed55cce920cb9b9e733e31a31788629efe452a1
Jim <compositr@compositr.dev>
2022-07-11 16:22:13 +1000

    chore: add tailwind
    Signed-off-by: Jim <compositr@compositr.dev>

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,jsx,md,mdx,ts,tsx}",
+    "./src/modules/**/*.{js,jsx,md,mdx,ts,tsx}",
+    "./src/common/**/*.{js,jsx,md,mdx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
