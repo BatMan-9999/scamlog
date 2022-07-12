47ad1a18ea6885264ee39618b2f853719112243b
Jim <compositr@compositr.dev>
2022-07-12 18:39:27 +1000

    fix: incorrect typing
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/modules/auth/types/next-auth.d.ts
+++ src/modules/auth/types/next-auth.d.ts
@@ -6,6 +6,6 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   type Session = DefaultSession & {
-    admin: boolean | AdminUser;
+    admin: false | AdminUser;
   };
 }
