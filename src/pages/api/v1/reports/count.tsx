2c591c54bf5986c19ba3726d85a2610d37218d1f
Jim <compositr@compositr.dev>
2022-07-12 18:40:18 +1000

    feat: count api routes
    Signed-off-by: Jim <compositr@compositr.dev>

---
+++ src/pages/api/v1/reports/count.tsx
@@ -0,0 +1,17 @@
+import { StandardAPIResponse } from "@/common/types/api/StandardAPIResponse";
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/utils/prisma";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<StandardAPIResponse<number>>
+) {
+  await prisma.$connect();
+
+  const result = await prisma.serverReport.count();
+
+  return res.status(200).json({
+    message: `Success`,
+    data: result,
+  });
+}
