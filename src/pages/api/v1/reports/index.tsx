8920d58ecd716e6a574a6e86b25bdf05ccb07565
Jim <compositr@compositr.dev>
2022-07-13 19:48:56 +1000

    feat: reports api
    Signed-off-by: Jim <compositr@compositr.dev>

---
+++ src/pages/api/v1/reports/index.tsx
@@ -0,0 +1,75 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/common/utilities/prisma";
+import { StandardAPIResponse } from "@/common/types/api/StandardAPIResponse";
+import { ScamServer, ServerReport } from "@prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<StandardAPIResponse<ServerReportResponse>>
+) {
+  if (req.method !== "GET")
+    return res.status(405).json({
+      message: "Method not allowed. Use GET",
+      data: null,
+    });
+
+  if (Array.isArray(req.query.cursor))
+    return res.status(400).json({
+      message: "Cursor must be a string",
+      data: null,
+    });
+
+  if (Array.isArray(req.query.id))
+    return res.status(400).json({
+      message: "ID must be a string",
+      data: null,
+    });
+
+  if (Array.isArray(req.query.name))
+    return res.status(400).json({
+      message: "Name must be a string",
+      data: null,
+    });
+
+  if (Array.isArray(req.query.serverId))
+    return res.status(400).json({
+      message: "ServerId must be a string",
+      data: null,
+    });
+
+  if (req.query.id && req.query.id?.length !== 24)
+    return res.status(400).json({
+      message: "ID must be 24 characters long",
+      data: null,
+    });
+
+  await prisma.$connect();
+  const query = await prisma.serverReport.findMany({
+    take: 10,
+    skip: req.query.cursor ? 1 : 0,
+    cursor: req.query.cursor ? { id: req.query.cursor } : undefined,
+
+    where: {
+      id: req.query.id || undefined,
+      name: req.query.name
+        ? {
+            contains: req.query.name,
+          }
+        : undefined,
+      serverId: req.query.serverId || undefined,
+    },
+  });
+
+  return res.status(200).json({
+    message: "Success",
+    data: {
+      servers: query,
+      cursor: query.at(-1)?.id,
+    },
+  });
+}
+
+export interface ServerReportResponse {
+  servers: ServerReport[];
+  cursor?: string;
+}
---
+++ src/pages/api/v1/reports/new.tsx
@@ -0,0 +1,153 @@
+import { StandardAPIResponse } from "@/common/types/api/StandardAPIResponse";
+import checkPerms from "@/modules/auth/permissions/functions/checkPerms";
+import { ScamServer, ServerReport } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { opts } from "../../auth/[...nextauth]";
+import { prisma } from "@/common/utilities/prisma";
+import { APIInvite } from "discord-api-types/v10";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<StandardAPIResponse<ServerReport | undefined>>
+) {
+  if (req.method !== "POST")
+    return res.status(405).json({
+      message: "Method not allowed. Use POST",
+      data: null,
+    });
+
+  const session = await unstable_getServerSession(req, res, opts);
+
+  if (!session)
+    return res.status(401).json({
+      message: "Unauthorized",
+      data: null,
+    });
+
+  // User is authenticated and can now access API
+  if (!req.body)
+    return res.status(400).json({
+      message: "Missing body",
+      data: null,
+    });
+
+  if (!Array.isArray(req.body.inviteCodes) || !req.body.inviteCodes?.length)
+    return res.status(400).json({
+      message: "Missing inviteCodes",
+      data: null,
+    });
+
+  if (
+    !["QR", "FAKENITRO", "OAUTH", "VIRUS", "NSFW", "SPAM", "OTHER"].includes(
+      req.body.serverType
+    )
+  )
+    return res.status(400).json({
+      message:
+        "serverType must be one of the following: QR, FAKENITRO, OAUTH, VIRUS, NSFW, SPAM, OTHER",
+      data: null,
+    });
+  await prisma?.$connect();
+
+  if (session.id) {
+    const isBanned = await prisma.bannedUser.findFirst({
+      where: {
+        userId: session.id,
+      },
+    });
+    if (isBanned)
+      return res.status(403).json({
+        message: "You are banned",
+        data: null,
+      });
+
+    const user = await prisma.user.findFirst({
+      where: {
+        id: session.id,
+      },
+    });
+
+    if (user) {
+      if (user.reportCoolDown) {
+        if (user.reportCoolDown.getTime() > Date.now())
+          return res.status(429).json({
+            message: `Too fast! You can report again in ${
+              user.reportCoolDown.getTime() - Date.now()
+            }ms`,
+            data: null,
+          });
+      }
+    }
+  }
+
+  // Fetch data from Discord
+  const invite: APIInvite | null = await fetch(
+    `https://discord.com/api/v10/invites/${req.body.inviteCodes[0]}?with_counts=true`
+  ).then((res) => res.json());
+
+
+  if (!invite?.guild)
+    return res.status(404).json({
+      message: "Invite not found",
+      data: null,
+    });
+
+  const dupe = await prisma.serverReport.findFirst({
+    where: {
+      serverId: invite.guild.id,
+    },
+  });
+
+  const dbDupe = await prisma.scamServer.findFirst({
+    where: {
+      serverId: invite.guild.id,
+    },
+  });
+
+  if (dupe || dbDupe)
+    return res.status(409).json({
+      message: "Server ID already exists",
+      data: null,
+    });
+
+  const returned = await prisma?.serverReport.create({
+    data: {
+      createdByUser: {
+        connect: {
+          id: session.id,
+        },
+      },
+      memberCount: invite.approximate_member_count!,
+      name: invite.guild.name,
+      verificationLevel: 0,
+      inviteCodes: req.body.inviteCodes,
+      serverId: invite.guild.id,
+      adminIds: req.body.adminIds ?? [],
+      bannerHash: invite.guild.banner,
+      description: invite.guild.description,
+      evidenceLinks: req.body.evidenceLinks ?? [],
+      isActive: true,
+      longReport: req.body.longReport,
+      nsfw: req.body.nsfw,
+      serverType: req.body.serverType,
+      iconHash: invite.guild.icon,
+    },
+  });
+
+  if (session.id)
+    prisma.user.update({
+      where: {
+        id: session.id,
+      },
+      data: {
+        // Cooldown is 2 minutes
+        reportCoolDown: new Date(Date.now() + 120_000),
+      },
+    });
+
+  res.status(201).json({
+    message: "Created",
+    data: returned,
+  });
+}
