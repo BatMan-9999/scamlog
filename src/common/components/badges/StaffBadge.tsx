0452f51da1adb436bc99ee50c74f71fb12a6288b
Jim <compositr@compositr.dev>
2022-07-14 21:40:49 +1000

    feat: align middle badge
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/common/components/badges/StaffBadge.tsx
+++ src/common/components/badges/StaffBadge.tsx
@@ -2,7 +2,7 @@ import { Shield } from "react-feather";
 
 export default function StaffBadge() {
   return (
-    <span className="badge badge-primary py-4">
+    <span className="align-middle badge badge-primary py-4">
       <Shield className="align-middle" />
       <span className="ml-1 align-middle">Staff</span>
     </span>
