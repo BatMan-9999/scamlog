f23f91b9b13ac25461e81b09d979f50809fc5707
Jim <compositr@compositr.dev>
2022-07-14 21:57:53 +1000

    fix: smol badge
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/common/components/badges/RootBadge.tsx
+++ src/common/components/badges/RootBadge.tsx
@@ -2,8 +2,8 @@ import { Settings } from "react-feather";
 
 export default function RootBadge() {
   return (
-    <span className="badge badge-accent py-4">
-      <Settings className="align-middle" />
+    <span className="badge badge-sm badge-accent py-2">
+      <Settings className="align-middle" height={15} width={15} />
       <span className="ml-1 align-middle">Root</span>
     </span>
   );
--- src/common/components/badges/StaffBadge.tsx
+++ src/common/components/badges/StaffBadge.tsx
@@ -2,8 +2,8 @@ import { Shield } from "react-feather";
 
 export default function StaffBadge() {
   return (
-    <span className="align-middle badge badge-primary py-4">
-      <Shield className="align-middle" />
+    <span className="align-middle badge badge-sm badge-primary py-2">
+      <Shield width={15} height={15} className="align-middle" />
       <span className="ml-1 align-middle">Staff</span>
     </span>
   );
