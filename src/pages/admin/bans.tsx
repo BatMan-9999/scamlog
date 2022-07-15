8fa4a259e627a4dfb8cdeb05b4a6bd00ba98fb00
Jim <compositr@compositr.dev>
2022-07-15 10:47:50 +1000

    feat: bans; coming soon!
    Signed-off-by: Jim <compositr@compositr.dev>

---
+++ src/pages/admin/bans.tsx
@@ -0,0 +1,13 @@
+import FlexCenter from "@/common/components/base/FlexCenter";
+import DrawerLayout from "@/modules/dash/components/DrawerLayout";
+
+export default function Bans() {
+  return (
+    <DrawerLayout>
+      <FlexCenter>
+        <h1 className="text-2xl font-bold">Coming Soon!</h1>
+        <span className="text-xl">In the meantime, manage users through the Users tab</span>
+      </FlexCenter>
+    </DrawerLayout>
+  );
+}
