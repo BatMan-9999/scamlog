2126c5c81a5eb32026dacba2e9804b87889a39d8
Jim <compositr@compositr.dev>
2022-07-15 14:36:04 +1000

    feat: ISR index
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
 import ScamServerCard from "@/modules/search/components/display/ScamServerCard";
 import ScamServersContainer from "@/modules/search/components/display/ScamServersDisplay";
 import { ScamServer } from "@prisma/client";
+import { GetStaticProps } from "next";
 import { useInfiniteQuery } from "react-query";
+import { prisma } from "@/common/utilities/prisma";
+import { useState } from "react";
 
-export default function Index() {
+/**
+ * How this page works:
+ *
+ * Fetch the first page of scam servers from the database, and display them statically
+ * When the user clicks Load More, begin using the API to load them
+ */
+
+export default function Index({
+  scamServers,
+  initialCursor,
+}: {
+  scamServers: ScamServer[];
+  initialCursor: string;
+}) {
+  const [loadedFromAPI, setLoadedFromAPI] = useState(false);
+
   const fetchScamServer = ({ pageParam }: { pageParam?: string }) =>
     fetch(`/api/v1/servers${pageParam ? `?cursor=${pageParam}` : ""}`).then(
       (res) => res.json()
@@ -11,19 +29,20 @@ export default function Index() {
 
   const {
     data,
-    error,
     fetchNextPage,
     hasNextPage,
-    isFetching,
-    isFetchingNextPage,
     status,
   } = useInfiniteQuery("scamservers", fetchScamServer, {
     getNextPageParam: (lastPage) => lastPage?.data?.cursor ?? null,
+    enabled: loadedFromAPI,
   });
   return (
     <>
       <div className="flex flex-col justify-center items-center">
         <ScamServersContainer>
+          {scamServers.map((server) => (
+            <ScamServerCard key={server.id} {...server} />
+          ))}
           {data?.pages.map((page) =>
             page?.data?.servers?.map?.((server: ScamServer) => (
               <ScamServerCard key={server.id} {...server} />
@@ -32,16 +51,53 @@ export default function Index() {
         </ScamServersContainer>
       </div>
       <div className="flex flex-col items-center justify-center">
-        <button
-          onClick={() => fetchNextPage()}
-          className={`btn btn-primary btn-wide ${
-            status === "loading" ? "loading" : ""
-          }`}
-          disabled={!hasNextPage}
-        >
-          {hasNextPage ? "Load more" : "You've reached the end!"}
-        </button>
+        {loadedFromAPI ? (
+          <button
+            onClick={() => {
+              fetchNextPage();
+              setLoadedFromAPI(true);
+            }}
+            className={`btn btn-primary btn-wide ${
+              status === "loading" ? "loading" : ""
+            }`}
+            disabled={!hasNextPage}
+          >
+            {hasNextPage ? "Load more" : "You've reached the end!"}
+          </button>
+        ) : (
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              setLoadedFromAPI(true);
+              fetchNextPage({
+                pageParam: initialCursor,
+              });
+            }}
+          >
+            Load More
+          </button>
+        )}
       </div>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  await prisma.$connect();
+
+  const scamServers = await prisma.scamServer.findMany({
+    take: 10,
+  });
+
+  return {
+    props: {
+      scamServers: scamServers.map((server) => ({
+        ...server,
+        createdAt: server.createdAt.toISOString(),
+        updatedAt: server.updatedAt.toISOString(),
+      })),
+      initialCursor: scamServers[scamServers.length - 1].id,
+    },
+    revalidate: 5,
+  };
+};
