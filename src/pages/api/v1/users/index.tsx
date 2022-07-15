7efd5993b366378f5156f484ce919a104ac4a8a1
Jim <compositr@compositr.dev>
2022-07-15 11:42:57 +1000

    fix: import prisma
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/pages/api/v1/users/index.tsx
+++ src/pages/api/v1/users/index.tsx
@@ -7,6 +7,7 @@ import {
 import { NextApiRequest, NextApiResponse } from "next";
 import { unstable_getServerSession } from "next-auth";
 import { opts } from "../../auth/[...nextauth]";
+import { prisma } from "@/common/utilities/prisma";
 
 export default async function handler(
   req: NextApiRequest,
