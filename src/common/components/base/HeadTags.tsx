59cf2759d054fd13a60303539a004d19fbbd6be9
Jim <compositr@compositr.dev>
2022-07-16 12:02:23 +1000

    feat: meta tags
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/common/components/base/HeadTags.tsx
+++ src/common/components/base/HeadTags.tsx
@@ -14,10 +14,24 @@ export default function HeadTags() {
         // lol github copilot
         content="Scam Log is a community-driven database of known Discord scam servers. It is a free service that anyone can use to keep track of known scam servers, and report discovered ones."
       />
+      <meta name="twitter:image" content="/static/img/favicon.png" />
+
+      <meta name="theme-color" content="#4758e8" />
+
+      {/* Open Graph */}
+      
       <meta
-        name="twitter:image"
-        content="/static/img/favicon.png"
+        property="og:title"
+        content="Scam Log - A community-driven database of known scam servers"
       />
+      <meta property="og:site_name" content="Scam Log" />
+      <meta property="og:url" content="https://scamlog.compositr.dev" />
+      <meta
+        property="og:description"
+        content="Scam Log is a community-driven database of known Discord scam servers. It is a free service that anyone can use to keep track of known scam servers, and report discovered ones."
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/static/img/favicon.png" />
     </>
   );
 }
