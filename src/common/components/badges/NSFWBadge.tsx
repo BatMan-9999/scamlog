eac85408a4d7796ecd2d6dd569c817ad6b83c5cf
Jim <compositr@compositr.dev>
2022-07-16 19:47:01 +1000

    feat: make nsfw badge smol
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/common/components/badges/NSFWBadge.tsx
+++ src/common/components/badges/NSFWBadge.tsx
@@ -2,8 +2,8 @@ import { AlertTriangle } from "react-feather";
 
 export default function NSFWBadge() {
   return (
-    <span className="align-middle badge badge-warning py-4">
-      <AlertTriangle className="align-middle" />
+    <span className="align-middle badge badge-sm badge-warning py-2 ml-2">
+      <AlertTriangle className="align-middle"  height={15} width={15} />
       <span className="ml-1 align-middle">NSFW</span>
     </span>
   );
