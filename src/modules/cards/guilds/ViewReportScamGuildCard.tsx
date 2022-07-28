be701ec79559a0f7255c3036af81d4c557cddeca
Jim <compositr@compositr.dev>
2022-07-28 21:53:01 +1000

    fix: card links
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/modules/cards/guilds/ViewReportScamGuildCard.tsx
+++ src/modules/cards/guilds/ViewReportScamGuildCard.tsx
@@ -7,13 +7,15 @@ import BaseScamGuildCard from "./BaseScamGuildCard";
 export function ViewReportScamGuildCard({ ...server }: ScamServer) {
   return (
     <BaseScamGuildCard server={server}>
-      <Link href={`/servers/${server.id}`}>
-        <CardBottomAction>
-          <span className="font-semibold">
-            <span className="inline-block align-middle">View Report</span>
-            {server.nsfw ? <NSFWBadge /> : null}
-          </span>
-        </CardBottomAction>
+      <Link href={`/servers/${server.id}`} passHref replace>
+        <a>
+          <CardBottomAction>
+            <span className="font-semibold">
+              <span className="inline-block align-middle">View Report</span>
+              {server.nsfw ? <NSFWBadge /> : null}
+            </span>
+          </CardBottomAction>
+        </a>
       </Link>
     </BaseScamGuildCard>
   );
