53a7dade05a3455442b49f7cafaf817bfaf11894
Jim <compositr@compositr.dev>
2022-07-28 22:17:31 +1000

    feat: scroll & search
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { ViewReportScamGuildCard } from "@/modules/cards/guilds/ViewReportScamGuildCard";
 import FlexCenter from "@/common/components/base/flex/FlexCenter";
 import FlexGrid from "@/common/components/base/flex/FlexGrid";
+import InfiniteScroll from "react-infinite-scroller";
+import useDebounce from "@/common/hooks/useDebounce";
 
 /**
  * How this page works:
@@ -13,13 +15,16 @@ import FlexGrid from "@/common/components/base/flex/FlexGrid";
  */
 
 export default function Index() {
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
+
   const fetchScamServer = ({ pageParam }: { pageParam?: string }) =>
-    fetch(`/api/v1/servers${pageParam ? `?cursor=${pageParam}` : ""}`).then(
+    fetch(`/api/v1/servers${pageParam ? `?cursor=${pageParam}&` : "?"}name=${search}`).then(
       (res) => res.json()
     );
 
   const { data, fetchNextPage, hasNextPage, status } = useInfiniteQuery(
-    "scamservers",
+    ["scamservers", debouncedSearch],
     fetchScamServer,
     {
       getNextPageParam: (lastPage) => lastPage?.data?.cursor ?? null,
@@ -28,27 +33,31 @@ export default function Index() {
   return (
     <>
       <FlexCenter>
-        <FlexGrid>
-          {data?.pages.map((page) =>
-            page?.data?.servers?.map?.((server: ScamServer) => (
-              <ViewReportScamGuildCard key={server.id} {...server} />
-            ))
-          )}
-        </FlexGrid>
+        <div className="form-input mx-2 md:mx-6 my-4">
+          <input
+            type="text"
+            placeholder="Search Name..."
+            className="input input-bordered w-72 md:w-96"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
       </FlexCenter>
-      <FlexCenter>
-        <button
-          onClick={() => {
-            fetchNextPage();
-          }}
-          className={`btn btn-primary btn-wide ${
-            status === "loading" ? "loading" : ""
-          }`}
-          disabled={!hasNextPage}
-        >
-          {hasNextPage ? "Load more" : "You've reached the end!"}
-        </button>
-      </FlexCenter>
+      <InfiniteScroll
+        loadMore={() => fetchNextPage()}
+        hasMore={hasNextPage}
+        loader={<div>Loading...</div>}
+      >
+        <FlexCenter>
+          <FlexGrid>
+            {data?.pages.map((page) =>
+              page?.data?.servers?.map?.((server: ScamServer) => (
+                <ViewReportScamGuildCard key={server.id} {...server} />
+              ))
+            )}
+          </FlexGrid>
+        </FlexCenter>
+      </InfiniteScroll>
     </>
   );
 }
